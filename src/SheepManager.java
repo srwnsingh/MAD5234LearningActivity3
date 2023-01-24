63f548db5cb7183f3593f276e33688dc987bc0c0
sarwan singh <C0853452@mylambton.ca>
2023-01-24 16:00:58 -0500

    Learning activity 4 adding method to generate 1000000 sheep

--- src/SheepManager.java
+++ src/SheepManager.java
@@ -1,22 +1,34 @@
 import java.util.ArrayList;
-
+import java.util.Random;
 
 public class SheepManager {
 
     public static ArrayList<Sheep> arrayList = new ArrayList<>();
 
     public int AddSheep(Sheep sheepToAdd) {
-        // return number of sheep in the Database
-        int numberOfSheep = 0;
         arrayList.add(sheepToAdd);
         System.out.println("Added a sheep " + sheepToAdd.SheepName);
-        return numberOfSheep;
+        return arrayList.size();
     }
+
+    public static void generateSheep(int numberOfSheep) {
+        Random random = new Random();
+        for (int i = 0; i < numberOfSheep; i++) {
+            String name = "Sheep" + i;
+            String color = "Color" + random.nextInt(10);
+            int weight = random.nextInt(200);
+            int height = random.nextInt(100);
+            Sheep sheep = new Sheep(name, color, weight, height);
+            arrayList.add(sheep);
+            System.out.println("Sheep entry: " + sheep.toString());
+        }
+    }
+
 
     public static void main(String[] args) {
         SheepManager sheepManager = new SheepManager();
-        sheepManager.AddSheep(new Sheep("Prashant","Red",172,75));
+        generateSheep(1000000);
+        System.out.println("Number of sheep: " + arrayList.size());
     }
-
-
 }
+
